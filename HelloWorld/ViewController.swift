f0a7fd9c996c1dd5b1955683c25f5a51e667beda
gagalady <ryoheikara90824@icloud.com>
2020-09-18 15:19:43 +0900

    HelloWorldのラベルを追加した。

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 國光陵平 on 2020/09/03.
+//  Copyright © 2020 國光陵平. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
